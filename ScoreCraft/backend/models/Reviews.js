1348ae4977c84ced141ada6ab41b71a9a9c96814
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-13 19:10:32 -0300

    Update Reviews.js

--- ScoreCraft/backend/models/Reviews.js
+++ ScoreCraft/backend/models/Reviews.js
@@ -12,7 +12,7 @@ const reviewsSchema = new mongoose.Schema({
 });
 
 function getNextSequenceValue(sequenceName) {
-    var sequenceDocument = db.counters.findAndModify({
+    var sequenceDocument = ScoreCraftData.counters.findAndModify({
       query: { _id: sequenceName },
       update: { $inc: { sequence_value: 1 } },
       new: true
