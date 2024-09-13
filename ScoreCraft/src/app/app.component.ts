648eb805c8f77ab18df080107e5aab7d8eb7fbaf
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-13 19:04:33 -0300

    .

--- ScoreCraft/src/app/app.component.ts
+++ ScoreCraft/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { SignupComponent } from './signup/signup.component';
 import { HttpClient, provideHttpClient} from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,12 @@ import { HttpClient, provideHttpClient} from '@angular/common/http';
 })
 export class AppComponent {
   title = 'ScoreCraft';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Código que acessa o document ou window
+    }
+}
 }
