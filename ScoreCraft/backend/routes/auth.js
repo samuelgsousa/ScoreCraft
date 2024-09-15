56e061b098366986354ce69e2d6b97d5e801b229
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-15 20:40:34 -0300

    Update auth.js

--- ScoreCraft/backend/routes/auth.js
+++ ScoreCraft/backend/routes/auth.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Profile = require('../models/Profile'); // Certifique-se de que o caminho está correto
 
-router.get('/', async (req, res) => {
+router.get('/login', async (req, res) => {
     try {
-        res.json(1500);
+      const profiles = await Profile.find();
+      res.json(profiles);
     } catch (err) {
-        res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
-})
+  });
 // Rota para login
-router.post('/login', async (req, res) => {
-    res.json(1500)
-})
-// router.post('/login', async (req, res) => {
-//     const { email, senha } = req.body;
 
-//     try {
-//         // Buscar o usuário pelo email
-//         const user = await Profile.findOne({ email });
-//         if (!user) {
-//             return res.status(404).json({ message: 'Usuário não encontrado' });
-//         }
+ router.post('/login', async (req, res) => {
+     const { email, senha } = req.body;
 
-//         // Verificar a senha
-//         const isMatch = await user.compararSenha(senha);
-//         if (!isMatch) {
-//             return res.status(401).json({ message: 'Credenciais inválidas' });
-//         }
+     try {
+          //Buscar o usuário pelo email
+         const user = await Profile.findOne({ email });
+         if (!user) {
+             return res.status(404).json({ message: 'Usuário não encontrado' });
+         }
 
-//         // Se a senha estiver correta, retorne os dados do usuário (exceto a senha)
-//         const { senha: _, ...userData } = user.toObject(); // Excluir a senha da resposta
-//         res.json(userData);
-//     } catch (error) {
-//         res.status(500).json({ message: 'Erro no servidor', error });
-//     }
-// });
+          //Verificar a senha
+         const isMatch = await user.compararSenha(senha);
+         if (!isMatch) {
+             return res.status(401).json({ message: 'Credenciais inválidas' });
+         }
+
+          //Se a senha estiver correta, retorne os dados do usuário (exceto a senha)
+         const { senha: _, ...userData } = user.toObject();  //Excluir a senha da resposta
+         res.json(userData);
+     } catch (error) {
+         res.status(500).json({ message: 'Erro no servidor', error });
+     }
+ });
 
 module.exports = router;
