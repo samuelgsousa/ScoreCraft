8da2f7e3c2b7a1cd7d06ac33ba6b487ccbeac048
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-10-01 00:19:17 -0300

    Implementado Popover para exibir detalhes dos jogos

--- ScoreCraft/src/app/jogos/jogos.component.ts
+++ ScoreCraft/src/app/jogos/jogos.component.ts
@@ -4,19 +4,30 @@ import { CommonModule } from '@angular/common';
 import { Games } from '../interfaces/games';
 import { HttpClient} from '@angular/common/http';
 import { RouterLink } from '@angular/router';
+import { NgbModule, NgbPopover } from '@ng-bootstrap/ng-bootstrap';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
+import { ClickOutsideDirective } from './click-outside.directive'; 
 
 @Component({
   selector: 'app-jogos',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgbPopoverModule, NgbModule],
   templateUrl: './jogos.component.html',
   styleUrl: './jogos.component.css'
 })
+
+
 export class JogosComponent {
+
+  
+  @Output() clickOutside = new EventEmitter<void>();
 
   gameList: Games[] = [];
   
   constructor(private gamesService: GamesService) {}
+
+
 
   ngOnInit(): void {
     this.gamesService.getAllGames().subscribe(
@@ -31,3 +42,5 @@ export class JogosComponent {
   }
 
 }
+
+
