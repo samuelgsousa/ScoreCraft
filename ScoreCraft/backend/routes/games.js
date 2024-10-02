13c542be193b1695e7565cfce249eaecc4eb4fec
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-10-01 23:53:18 -0300

    Corrigido detalhes do CSS
    Also: endPoint antes de implementar função de busca

--- ScoreCraft/backend/routes/games.js
+++ ScoreCraft/backend/routes/games.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const Game = require('../models/Games'); 
-const axios = require('axios'); // Importar axios para fazer requisições HTTP
+const axios = require('axios');
 
 const IGDB_API_URL = 'https://api.igdb.com/v4/games'; // URL da API
 const IGDB_API_KEY = 'yqfxo07jpz3d01hcccr0e6ffqcce2l'; //chave da IGDB
